d323226673f09102bfe498b5435e9a96cd5e23d2
Shvam Singh <singhshivam583@yahoo.in>
2023-12-03 18:39:08 +0530

    update

---
+++ 03/Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import './App.css'
+import  { Footer, Header }  from './components'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Outlet/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ 03/Frontend/src/pages/pages.js
@@ -0,0 +1,13 @@
+import Login from "./Login";
+import Home from "./Home";
+import Register from "./Register";
+import About from "./About";
+import Contact from "./Contact";
+
+export {
+    Login,
+    Home,
+    Register,
+    About,
+    Contact
+}
